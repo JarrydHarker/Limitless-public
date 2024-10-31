168a184b7a1b9baa80fbb4fbe3bda9b3713ea008
lab_services_student <St10132642@vcconnect.edu.za>
2024-10-31 12:56:50 +0200

    nav button to the AI Page

--- app/src/main/java/com/example/limitless/ai.kt
+++ app/src/main/java/com/example/limitless/ai.kt
@@ -1,10 +1,13 @@
 package com.example.limitless
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.cardview.widget.CardView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +37,16 @@ class ai : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ai, container, false)
+        val view = inflater.inflate(R.layout.fragment_ai, container, false)
+
+        val aibtn: Button = view.findViewById(R.id.AINav)
+
+        aibtn.setOnClickListener{
+            val intent = Intent(requireActivity(), AI_Page::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 
     companion object {
